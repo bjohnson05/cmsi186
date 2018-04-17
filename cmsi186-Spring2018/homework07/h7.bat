d3dd48f26d4c28eeccd0d7b2aeada5cc66c38a61
B.J. Johnson <bjohnson05@gmail.com>
2018-04-17 09:20:39 -0700

    algorithm and batch files

---
+++ cmsi186-Spring2018/homework07/h7.bat
@@ -0,0 +1,21 @@
+
+
+
+
+@echo off
+@del /Q *.class
+@javac  *.java
+@del testresults.txt
+@rmdir /Q /S docs
+@mkdir docs
+@copy *.java docs\.
+@pushd docs
+@javadoc *.java
+@popd
+@start "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" docs\index.html
+
+@java TupleTestHarness >> testresults.txt
+@java DynamicChangemakerTestHarness >> testresults.txt
+
+
+
