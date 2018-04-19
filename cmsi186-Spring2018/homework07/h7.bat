cf653926a35f967937802550a1afbee86cfc13bc
B.J. Johnson <bjohnson05@gmail.com>
2018-04-19 13:25:02 -0700

    new shell script and batch files

--- cmsi186-Spring2018/homework07/h7.bat
+++ cmsi186-Spring2018/homework07/h7.bat
@@ -1,21 +1,26 @@
 
-
-
-
-@echo off
+@echo "cleaning up for build....."
 @del /Q *.class
 @javac  *.java
 @del testresults.txt
+
+@echo "creating javadocs....."
 @rmdir /Q /S docs
 @mkdir docs
-@copy *.java docs\.
+@copy  DynamicChangeMaker.java docs\.
+@copy  DynamicChangemakerTestHarness.java docs\.
+@copy  Tuple.java docs\.
+@copy  TupleTestHarness.java docs\.
 @pushd docs
 @javadoc *.java
 @popd
 @start "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" docs\index.html
 
+@echo "running test harnesses for evaluation....."
 @java TupleTestHarness >> testresults.txt
 @java DynamicChangemakerTestHarness >> testresults.txt
 
 
-
+@echo
+@echo ...........done..............
+@echo
