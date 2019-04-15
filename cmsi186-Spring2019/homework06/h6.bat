ebe9f555d1846e4a25576e4df48736a78b32d26c
B.J. Johnson <bjohnson05@gmail.com>
2019-04-15 11:09:19 -0700

    updated test scripts and tester java files

--- cmsi186-Spring2019/homework06/h6.bat
+++ cmsi186-Spring2019/homework06/h6.bat
@@ -24,33 +24,33 @@ echo . >> testresults.txt
 echo . >> testresults.txt
 
 @echo Running Collatz with 3691578348615318
-@echo Expecting 314 steps
+@echo Expecting 326 steps
 echo Running Collatz with 3691578348615318 >> testresults.txt
-echo Expecting 314 steps >> testresults.txt
+echo Expecting 326 steps >> testresults.txt
 java Collatz 3691578348615318 >> testresults.txt
 
-@echo Running Collatz with 147258369 
-@echo Expecting 107 steps
+@echo Running Collatz with 147258369
+@echo Expecting 216 steps
 echo Running Collatz with 147258369 >> testresults.txt
-echo Expecting 107 steps >> testresults.txt
+echo Expecting 216 steps >> testresults.txt
 java Collatz 147258369 >> testresults.txt
 
-@echo Running Collatz with 159482673357869421789456123 
-@echo Expecting 654 steps
+@echo Running Collatz with 159482673357869421789456123
+@echo Expecting 405 steps
 echo Running Collatz with 159482673357869421789456123 >> testresults.txt
-echo Expecting 654 steps >> testresults.txt
+echo Expecting 405 steps >> testresults.txt
 java Collatz 159482673357869421789456123 >> testresults.txt
 
-@echo Running Collatz with 75942615348675395174185296348152659 
-@echo Expecting 776 steps
+@echo Running Collatz with 75942615348675395174185296348152659
+@echo Expecting 478 steps
 echo Running Collatz with 75942615348675395174185296348152659 >> testresults.txt
-echo Expecting 776 steps >> testresults.txt
+echo Expecting 478 steps >> testresults.txt
 java Collatz 75942615348675395174185296348152659 >> testresults.txt
 
-@echo Running Collatz with 7594261534867539517418529634815265910200201075476883366222929291818181823413747890 
-@echo Expecting 951 steps
+@echo Running Collatz with 7594261534867539517418529634815265910200201075476883366222929291818181823413747890
+@echo Expecting 567 steps
 echo Running Collatz with 7594261534867539517418529634815265910200201075476883366222929291818181823413747890 >> testresults.txt
-echo Expecting 951 steps >> testresults.txt
+echo Expecting 567 steps >> testresults.txt
 java Collatz 7594261534867539517418529634815265910200201075476883366222929291818181823413747890 >> testresults.txt
 
 @echo
