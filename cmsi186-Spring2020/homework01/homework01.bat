6526f72a60635441380eb97d971a981e0f2e986d
B.J. Johnson <bjohnson05@gmail.com>
2020-01-26 21:18:19 -0800

    changed locations of lots of files

---
+++ cmsi186-Spring2020/homework01/homework01.bat
@@ -0,0 +1,131 @@
+@echo off
+
+@echo checking Behrens
+@pushd BehrensKaitlyn\CMSI-186_Projects\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking Carson
+@pushd CarsonErin\CMSI-186_Projects\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking Cassellius
+@pushd CasselliusSeth\CMSI-186_Projects\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking Hendrick
+@pushd HendrickMason\CMSI-186_Projects\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking Huerta
+@pushd HuertaEmilia\CMSI_186_Emilia\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking Irving
+@pushd IrvingZachary\CMSI-186_Projects_ZachIrving\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking Jensen
+@pushd JensenJoseph\CMSI-186_Projects\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking Kane
+@pushd KaneLouis\cmsi186_Kane\CMSI-186_Projects\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking Kublin
+@pushd KublinHenno\CMSI186_HennoKublin\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking Lancaster
+@pushd LancasterHarry\CMSI186_Harry\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking LinBianco
+@pushd LinBiancoFosse\CMSI-186_Projects\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking Longo
+@pushd LongoJacob\CMSI-186_Projects\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking Marfice
+@pushd MarficeJack\CMSI186_Marfice\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking Mellacheruvu
+@pushd MellacheruvuAmeya\CMSI-186_Ameya\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking O'Donnell
+@pushd "ODonnellAidan\CMSI-186_AidanO\homework 01"
+@call evalHW01
+@popd
+@pause
+
+@echo checking Ornelas
+@pushd OrnelasCristian\CMSI-186_Projects\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking Reyes
+@pushd ReyesMegan\CMSI186_MeganReyes\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking Richardson
+@pushd RichardsonMegan\CMSI-186_Projects\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking Slanec
+@pushd SlanecNick\NickSlanecCMSI186_Projects\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking West
+@pushd WestMegan\CMSI_186_Megan_West\homework01
+@call evalHW01
+@popd
+@pause
+
+@echo checking Young
+@pushd YoungCaden\CMSI-186_Projects\homework01
+@call evalHW01
+@popd
+@pause
+
+
+@echo
+@echo ....done....
