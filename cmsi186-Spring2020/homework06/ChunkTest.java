263707da7371abe87e890fcfe58f4de7dc8afa07
B.J. Johnson <bjohnson05@gmail.com>
2020-04-05 10:09:30 -0700

    added BrobIntLoopTest and new ChunkTest

--- cmsi186-Spring2020/homework06/ChunkTest.java
+++ cmsi186-Spring2020/homework06/ChunkTest.java
@@ -1,3 +1,22 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * File name  :  ChunkTest.java
+ * Purpose    :  Learning exercise to implement arbitrarily large numbers and their operations
+ * @author    :  B.J. Johnson
+ * Date       :  2017-04-04
+ * Description:  @see <a href='http://bjohnson.lmu.build/cmsi186web/homework06.html'>Assignment Page</a>
+ * Notes      :  None
+ * Warnings   :  None
+ *
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Revision History
+ * ================
+ *   Ver      Date     Modified by:  Reason for change or modification
+ *  -----  ----------  ------------  ---------------------------------------------------------------------
+ *  1.0.0  2019-04-22  B.J. Johnson  Initial writing and begin coding
+ *  1.0.1  2020-02-21  B.J. Johnson  Added file header
+ *  1.0.2  2020-04-05  B.J. Johnson  updated to add "toArray()" method
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
 import java.util.Arrays;
 
 /**
