71f7ba0627283508c6e64900d96f38b9fd37ceea
B.J. Johnson <bjohnson05@gmail.com>
2020-07-04 15:15:55 -0700

    add homework00 files

---
+++ cmsi186-Fall2020/homework00/homework00.bat
@@ -0,0 +1,132 @@
+@echo off
+
+@echo checking Behrens
+@pushd BehrensKaitlyn\CMSI-186_Projects\homework00
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking Carson
+@pushd CarsonErin\CMSI-186_Projects\homework00
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking Cassellius
+@pushd CasselliusSeth\CMSI-186_Projects\homework00
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking Hendrick
+REM @pushd HendrickMason\CMSI-186_Projects\homework00
+REM @javac *.java
+REM @java Greeter
+REM @popd
+
+@echo checking Huerta
+@pushd HuertaEmilia\CMSI_186_Emilia\homework00
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking Irving
+@pushd IrvingZachary\CMSI-186_Projects_ZachIrving\homework00
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking Jensen
+@pushd JensenJoseph\CMSI-186_Projects\homework00
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking Kane
+@pushd KaneLouis\cmsi186_Kane\CMSI-186_Projects\homework00
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking Kublin
+@pushd KublinHenno\CMSI186_HennoKublin\homework00
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking Lancaster
+@pushd LancasterHarry\CMSI186_Harry\homework00
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking LinBianco
+@pushd LinBiancoFosse\CMSI-186_Projects\homework00
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking Longo
+@pushd LongoJacob\CMSI-186_Projects\homework00
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking Marfice
+@pushd MarficeJack\CMSI186_Marfice\homework00
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking Mellacheruvu
+@pushd MellacheruvuAmeya\CMSI-186_Ameya\homework00
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking O'Donnell
+@pushd ODonnellAidan\CMSI-186_AidanO\homework 0
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking Ornelas
+@pushd OrnelasCristian\CMSI-186_Projects\homework00
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking Reyes
+@pushd ReyesMegan\CMSI186_MeganReyes\homework00
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking Richardson
+@pushd RichardsonMegan\CMSI-186_Projects\homework00
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking Slanec
+@pushd SlanecNick\NickSlanecCMSI186_Projects\homework00
+@javac *.java
+@java Greeter
+@popd
+
+@echo checking West
+REM @pushd WestMegan\CMSI_186_Megan_West\homework00
+REM @javac *.java
+REM @java Greeter
+REM @popd
+
+@echo checking Young
+@pushd YoungCaden\CMSI-186_Projects\homework00
+@javac *.java
+@java Greeter
+@popd
+
+
+
+@echo
+@echo ....done....
