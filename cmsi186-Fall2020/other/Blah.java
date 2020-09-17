f08e70dc8e2adebc8e74461cfd64385c45039607
B.J. Johnson <bjohnson05@gmail.com>
2020-09-17 09:37:47 -0700

    added example files to "other" directory

--- cmsi186-Fall2020/other/Blah.java
+++ cmsi186-Fall2020/other/Blah.java
@@ -1,1 +1,6 @@
-
+public class Blah {
+	public static void main( String args[] ) {
+		String blah = "BLAH";
+		System.out.println( (0 == "BLAH".compareTo( "BLAH" )) ? "got it" : "don't got it" );
+	}
+}
